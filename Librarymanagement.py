80d44f10828c4a633de8d8f3e7db3c5ee7e90652
Swetanki Goswami <swetankigoswami@gmail.com>
2025-06-06 21:36:58 +0530

    Add intial files

---
+++ Librarymanagement.py
@@ -0,0 +1,19 @@
+class Library:
+    def __init__(self):
+        self.noBooks = 0
+        self.books = []
+
+    def addBook(self,book):
+        self.books.append(book)
+        self.noBooks = len(self.books)
+
+    def showInfo(self):
+        print(f"The library has {self.noBooks} books. The books are")
+        for book in self.books:
+            print(book)
+
+l1 = Library()
+l1.addBook("Harry Potter")
+l1.addBook("Ikigai")
+l1.addBook("Rich Dad , Poor Dad")
+l1.showInfo()
